fe743187ae594e14f02ae8347cd9acd101a2066d
Kelly Ripple <kelly.m.ripple@gmail.com>
2024-12-06 19:17:02 -0500

    add skeleton cafes controller, unstyled

---
+++ app/controllers/cafes_controller.rb
@@ -0,0 +1,59 @@
+class CafesController < ApplicationController
+  def index
+    # @cafes = Cafe.all
+    # if request.xhr?
+    #   render @cafes
+    # else
+    #   render :index
+    # end
+
+    render :index
+  end
+
+  # def show
+  #   @cafe = Cafe.find_by(slug: params[:id])
+  #   @cafe = current_cafe unless @cafe
+  #   @menu_item = MenuItem.new
+  #   @menu_items = @cafe.menu_items
+  #   @charitable_gifts = @cafe.unredeemed_charitable_gifts
+  #   @unredeemed_coffee_gifts = @cafe.unredeemed_coffee_gifts
+  #   if request.xhr?
+  #     if params[:search] == ""
+  #       @unredeemed_coffee_gifts = []
+  #     else
+  #       @unredeemed_coffee_gifts = @cafe.search_non_charitable_gifts(params[:search]).order("created_at DESC")
+  #     end
+  #     render partial: "unredeemed_coffee_gift", collection: @unredeemed_coffee_gifts
+  #   end
+  # end
+
+  # def update
+  #   @cafe = current_cafe
+  #   if params[:cafe]
+  #     @cafe.update_attributes(params[:cafe])
+  #   else
+  #     flash[:error] = ["Something Went Wrong! Your Picture Was Not Uploaded"]
+  #   end
+  #   render :show
+  # end
+
+  # def borough
+  #   if request.xhr?
+  #     @cafes = Cafe.filter_by_borough(params[:tag])
+  #     render @cafes
+  #   else
+  #     @cafes = Cafe.all
+  #     render :index
+  #   end
+  # end
+
+  # def neighborhood
+  #   if request.xhr?
+  #     @cafes = Cafe.filter_by_neighborhood(params[:tag])
+  #     render @cafes
+  #   else
+  #     @cafes = Cafe.all
+  #     render :index
+  #   end
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,32 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'cafes#index'
+  
+  # get '/cafes/login' => 'cafe_sessions#new'
+  # get '/cafes/profile' => 'cafes#show'
+  # post '/cafes/borough' => 'cafes#borough'
+  # post '/cafes/neighborhood' => 'cafes#neighborhood'
+  # post '/coffee_gifts/filter' => 'coffee_gifts#filter'
+
+  # resources :cafes, only: [:index, :show, :update] do
+  #   resources :coffee_gifts, only: [:new, :create]
+  # end
+
+  # resources :menu_items, only: [:destroy, :create, :update]
+  # resources :users, only: [:create, :edit, :update]
+  # resources :coffee_gifts, only: [:show, :update]
+  # resources :transactions, only: [:new, :create]
+
+  # get '/login' => 'user_sessions#new'
+  # post '/users/sessions' => 'user_sessions#create'
+  # delete '/logout' => 'user_sessions#destroy'
+  # get '/profile' => 'users#show'
+  # get '/register' => 'users#new'
+  # get '/confirmation/:id' => 'coffee_gifts#confirm', as: "confirmation"
+
+  # get '/redemption_confirmation/:id' => 'coffee_gifts#confirm_redemption', as: "redemption_confirmation"
+
+  # post '/cafes/sessions' => 'cafe_sessions#create'
+  # delete '/cafes/logout' => 'cafe_sessions#destroy'
 end
